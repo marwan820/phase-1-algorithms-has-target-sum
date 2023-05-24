2ed27612526f4e642636b1404cfe53b0fcfe0d20
Marwan Rahim <marwan820@Marwan>
2023-05-24 18:32:08 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // 1 step
+  const seenNumbers = {};
+  for (const number of array) {
+    // n steps
+    const complement = target - number;
+    // n steps
+    if (seenNumbers[complement]) return true;
+    // n steps
+    seenNumbers[number] = true;
+  }
+  // 1 step
+  return false;
 }
+  
+/* create if statement if two elements from array add up to
+the target number retrun true else false */
+
+// integer array from first test case 
+
+
+// create an array of integers 
+//if array element pairs up to the target number 
+// return true
+// else return false 
+// would have to the array elements and add them up
+// create a hash table to store the values 
 
 /* 
   Write the Big O time complexity of your function here
@@ -12,7 +36,10 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
-*/
+/*The function should return true if 
+any pair of numbers in the array adds up to
+the target number.
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -30,5 +57,5 @@ if (require.main === module) {
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
-
+ */
 module.exports = hasTargetSum;
